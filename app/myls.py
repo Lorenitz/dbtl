b975da9e6ecd174ea3e6357fa4dd94f1bceeafa8
Lorena <lorena.g.saar@gmail.com>
2022-11-01 23:12:56 +0100

    Added desc help to command dbtl

--- app/myls.py
+++ app/myls.py
@@ -3,7 +3,19 @@ import sys
 
     
 if len (sys.argv) < 2:
-    print('show help')
+    help = """dbtl allows you to transform data in data warehouses
+
+usage:
+  dbtl [sub-command]
+
+sub-commands:
+  show    Show the transformation that will take place
+  lint    Lint SQL files using SQLFluff
+  run     Run all transformations
+  test    Test transformations in test environment
+    """
+    
+    print(help)
     sys.exit()
     
     
