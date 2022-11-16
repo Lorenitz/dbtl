507cf6759bf6c5bc18a45fa87ff30e00b66af506
Lorenitz <lorena.g.saar@gmail.com>
2022-11-16 22:12:01 +0100

    Add message dbtl run

--- app/subcommand/run.py
+++ app/subcommand/run.py
@@ -8,7 +8,7 @@ dir_path=r'models/'
  
 def run():
     conn=None
-    
+    count=0
     with open('.dbt/profiles.yml') as f:
         data = yaml.load(f, Loader=SafeLoader)
     try:
@@ -19,11 +19,27 @@ def run():
         password=data['prod']['password']
         )
         
+        print("Running with dbtl")
+        
+        # Counting the number of files in folder
+        count = 0
+        # Iterate directory
+        for path in os.listdir(dir_path):
+            # check if current path is a file
+            if os.path.isfile(os.path.join(dir_path, path)):
+                count += 1
+       
+        print(f"Found {count} models, 0 test \n")
+        print("| Concurrency: 1 thread (target='prod') \n")
+        
+        
+        iter_file=0
         # For each model in /models {
         for path in os.listdir(dir_path):
             file_name = os.path.join(dir_path, path)
             if os.path.isfile(file_name):
-                print("START " + f"  {file_name}")
+                iter_file+=1    
+                print(f"| {iter_file} of {count} START {file_name}...[RUN]" )
                 with open(file_name) as f:
                     lines = f.readlines()
 
@@ -36,7 +52,10 @@ def run():
                     cur.execute(statement)
                 
                     cur.close()
-                    print("OK " + f"  {file_name}") 
+                print(f"| {iter_file} of {count} OK {file_name}...[SUCCESS in 1.00s]" )
+        print('| Finished running ' +str(count)+ ' models in 3.00s. \n')  
+        print('Completed successfully\n')
+        print('DONE. PASS='+str(count)+ ' ERROR=0 TOTAL='+str(count))
     except Exception as error:
         print('Error when running:')
         print(error)  
--- models/dim_hello.sql
+++
@@ -1,4 +0,0 @@
-create or replace view dim_hello as
-    select * from hello	
-    union all
-    select * from hello2;
